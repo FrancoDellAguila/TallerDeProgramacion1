ff5e142fd17f742cc06d7589fcefd55ef6a451c4
FrancoDellAguila <francodellaguila@hotmail.com>
2021-08-29 19:53:50 -0300

    bases UI

--- TP1-1/src/main/java/com/taller1/TP1_1/UI.java
+++ TP1-1/src/main/java/com/taller1/TP1_1/UI.java
@@ -1,13 +1,117 @@
-package com.taller1.TP1_1;
+package vista;
 
-public class UI {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-	public void abrirLocal() {
-		int mesas;
-		
-		
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class UI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	public void cerrarMesa() {}
-	public void ocuparMesa() {}
-	
+
+	/**
+	 * Create the frame.
+	 */
+	public UI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Abrir local");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel.add(btnNewButton_2);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		panel_1.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Cerrar mesa");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblNewLabel_1);
+		
+		JPanel panel_5 = new JPanel();
+		panel_1.add(panel_5);
+		
+		textField_1 = new JTextField();
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_1.add(panel_6);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_1.add(btnNewButton_1);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2);
+		panel_2.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("Ocupar mesa");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(lblNewLabel_2);
+		
+		JPanel panel_7 = new JPanel();
+		panel_2.add(panel_7);
+		
+		textField_2 = new JTextField();
+		panel_2.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JPanel panel_8 = new JPanel();
+		panel_2.add(panel_8);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_2.add(btnNewButton);
+	}
+
 }
