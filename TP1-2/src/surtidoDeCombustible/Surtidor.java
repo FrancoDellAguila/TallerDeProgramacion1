feb9f27dafb6c6e661877c1d8dd12f913167eadb
Franco Chalop <38596776+FrancoChalop@users.noreply.github.com>
2021-09-05 19:18:37 -0300

    surtidor try 1

--- TP1-2/src/surtidoDeCombustible/Surtidor.java
+++ TP1-2/src/surtidoDeCombustible/Surtidor.java
@@ -1,5 +1,43 @@
 package surtidoDeCombustible;
 
 public class Surtidor {
+    private float cantidadCombustible;
 
+
+
+
+    public void inicializarSurtidor(float carga){
+        this.cantidadCombustible=0;
+		cargarSurtidor(carga);
+		this.acumuladoManguera1=0;
+		this.acumuladoManguera2=0;
+		this.ultimaventaManguera1=0;
+		this.ultimaventaManguera2=0;
+		this.activo1=false;
+		this.activo2=true;
+    }
+    public void cargaSurtidor(float carga){
+
+    }
+    public void descargaMangera1(){
+
+    }
+    public void activaMangera1(){
+
+    }
+    public float getExistenciaDeposito(){
+
+    }
+    public float getExistenciaDeposito(){
+        
+    }
+    public float getExistenciaDeposito(){
+        
+    }
+    public float getExistenciaDeposito(){
+        
+    }
+    public float getExistenciaDeposito(){
+        
+    }
 }
