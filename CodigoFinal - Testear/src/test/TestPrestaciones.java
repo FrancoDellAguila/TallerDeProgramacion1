0f56e56a080b08971bc57afe37ace7f34ff0bf94
tobias-andrade <tobiaseltoti5@gmail.com>
2021-11-25 16:09:56 -0300

    test prestaciones
    agregue la dependencia a pom.xml y agregue test de prestaciones (consultaMedica e Internaciones)

--- CodigoFinal - Testear/src/test/TestPrestaciones.java
+++ CodigoFinal - Testear/src/test/TestPrestaciones.java
@@ -6,6 +6,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import decorators.IMedico;
+import modelo.ConsultaMedica;
+import modelo.Habitacion;
+import modelo.Internacion;
+
+import modelo.Privada;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
 public class TestPrestaciones {
 
 	@Before
@@ -16,6 +28,24 @@ public class TestPrestaciones {
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void testConsultaMedica() {
+		int cant= 2;
+		IMedico medico = mock(IMedico.class);
+		when(medico.getHonorario()).thenReturn(10.0);
+		ConsultaMedica cons = new ConsultaMedica(cant, medico);
+		assertEquals(cons.getCantidad(), 2, 0);
+		assertEquals(cons.getSubtotal(), 1612,0);
+	}
 	
+	@Test
+	public void testInternacion() {
+		int cant=2;
+		Privada hab = mock(Privada.class);
+		when(hab.getCosto(cant)).thenReturn((double) 1000);
+		Internacion internacion = new Internacion(cant, hab);
+		assertEquals(internacion.getCantidad(), cant, 0);
+		assertEquals(internacion.getSubtotal(),1000,0);
+	}
 
 }
